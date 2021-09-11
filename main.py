b0f23cb36d7c80ae5c97754a9dc8043ef2cf518a
LuanZhanKuangGe <zhoubo616819598@gmail.com>
2021-09-11 12:57:42 +0800

    modify

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import re
+from gooey import Gooey, GooeyParser
 import scrapy
+from scrapy.crawler import CrawlerProcess
+
 
 def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"
@@ -13,21 +16,30 @@ class iwaraSpider(scrapy.Spider):
     def start_requests(self):
         urls = []
         for files in os.listdir("./"):
-            if files.endswith("source.mp4"):
+            if files.lower().endswith("source.mp4"):
                 url = 'https://www.iwara.tv/videos/'+ files.split("_")[1]
                 request = scrapy.Request(url=url, callback=self.parse)
                 request.cb_kwargs['oldname'] = "./" + files
                 yield request
-            elif files.endswith("Source.mp4.xltd"):
+            elif files.lower().endswith("Source.mp4.xltd"):
                 url = 'https://www.iwara.tv/videos/'+ files.split("_")[1]
                 print(url)
 
     def parse(self, response, oldname):
-        print("-----------------------------------------")
         newname = response.css('a.username::text').get() + " - " + response.css('h1.title::text').get()
         newname = validateTitle(newname)
         newname = "./" + newname + ".mp4"
-        print(oldname)
-        print(newname)
+        print(oldname + " -----> " + newname)
         os.rename(oldname,newname)
-        print("-----------------------------------------")
+
+@Gooey()
+def main():
+    parser = GooeyParser(description='Process some integers.')
+    parser.add_argument('filename', help="选择目标文件夹", widget='DirChooser')
+    parser.parse_args()
+
+if __name__ == '__main__':
+    main()
+    process = CrawlerProcess()
+    process.crawl(iwaraSpider)
+    process.start()
